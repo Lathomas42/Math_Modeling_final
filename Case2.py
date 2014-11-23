e6f72b9245438102117395e9c9468c7c1715331b
Lathomas42 <Lathomas42@gmail.com>
2014-11-23 13:20:24 -0500

    changed initial parameters

--- Case2.py
+++ Case2.py
@@ -4,12 +4,15 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def solveCase2(dt=1/100., t_max=10000, x_i=1.7, y_i=1.7, z_i=1.7,
-               x_c=1, a=1, b=1, c=1, d=1, e=1, f=1, g=1):
+def solveCase2(dt=1/100., t_max=10000, x_i=1.7, y_i=1.7, z_i=5,
+               x_c=1., a=1., b=1., c=1., d=1., e=1., f=3., g=1.):
     t = 0.
     x = x_i
     y = y_i
     z = z_i
+    x_lim = x_c*((2*a*g+b*f)/(b*f+a*g))
+    y_lim = (b*f + a*g)/(g*b)
+    z_lim = (1/(e*f*b))*(a*g + f*b - d*a*g*x_lim)
     x_list = [x]
     y_list = [y]
     z_list = [z]
@@ -29,17 +32,18 @@ def solveCase2(dt=1/100., t_max=10000, x_i=1.7, y_i=1.7, z_i=1.7,
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.plot(x_list, y_list, z_list, c=c_list)
+    ax.scatter([x_lim],[y_lim],[z_lim])
     plt.show()
 
 
-def timeCase2(dt=1/10., t_max=1000, x_i=1.7, y_i=1.7, z_i=1.7,
-              x_c=1, a=1, b=1, c=1, d=1, e=1, f=1, g=1):
+def timeCase2(dt=1/100., t_max=1000, x_i=1.7, y_i=1.7, z_i=5,
+              x_c=1, a=1, b=1, c=1, d=1, e=1, f=3, g=1):
     plt.ion()
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.set_xlim(1,2)
     ax.set_ylim(0,5)
-    ax.set_zlim(0,2)
+    ax.set_zlim(0,10)
     plt.show()
     t = 0.
     x = x_i
